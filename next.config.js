e0ed74e3f18e5c23596fdc35f03a604caf18ca3e
Gabriel <gabriel.tendongpk71@gmail.com>
2022-02-15 09:00:23 +0000

    update

--- next.config.js
+++ next.config.js
@@ -11,5 +11,18 @@ module.exports = {
   reactStrictMode: true,
   env: {
     baseUrl: "https://ztrain-shop.herokuapp.com"
-  }
+  },
+  images: {
+    domains: [
+      'static.monbottier.fr',
+      'shop.manfield.fr',
+      'uploads-ssl.webflow.com',
+      'sneakers123.s3.amazonaws.com',
+      's-media-cache-ak0.pinimg.com',
+      'i.pinimg.com',
+      'lookhomme.com',
+      'media.achat-ville.com',
+      'static2.chaussminimaxi.fr'
+    ],
+  },
 }
