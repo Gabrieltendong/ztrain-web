6961b743e0fc8b89eec73ed791bb5f49038f7004
Gabriel <gabitendong@gmail.com>
2023-01-05 09:43:32 +0100

    add privacy policy page

--- pages/privacy-policy/index.js
+++ pages/privacy-policy/index.js
@@ -3,10 +3,11 @@ const PrivacyPolicy = () => {
         <div >
             <h3 style={{paddingLeft: 50}}>POLITIQUE DE CONFIDENTIALITÉ</h3>
             <p style={{padding: 50}}>
-Vous vous apprêtez à utiliser l'application Ztrain de Zenity. Nous sommes ravis !
-Nous vous informons que son utilisation implique que nous collections et que nous traitions les données personnelles que vous allez nous communiquer (c’est-à-dire toutes celles que vous nous confierez). Ces informations collectées ne sont destinées qu’à Zenity, plus précisément à la formation. Nous nous engageons à ne jamais vendre ou louer ces données.
-Vous pourrez bien sûr, à tout moment, demander à consulter, rectifier voire supprimer vos données personnelles. Il vous suffit d’envoyer votre demande à contact@zenity.fr.
-Merci !</p>
+            Vous vous apprêtez à utiliser l'application Ztrain de Pawk. Nous sommes ravis !
+            Nous vous informons que son utilisation implique que nous collections et que nous traitions les données personnelles que vous allez nous communiquer (c’est-à-dire toutes celles que vous nous confierez). Ces informations collectées ne sont destinées qu’à Pawk, plus précisément à la formation. Nous nous engageons à ne jamais vendre ou louer ces données.
+            Vous pourrez bien sûr, à tout moment, demander à consulter, rectifier voire supprimer vos données personnelles. Il vous suffit d’envoyer votre demande à gabriel.tendongpk71@gmail.com
+            Merci !
+            </p>
         </div>
     )
 }
