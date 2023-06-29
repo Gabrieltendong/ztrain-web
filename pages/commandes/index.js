6739d94cde2e8d970e0b01112311b0ef23920d73
Gabriel <gabitendong@gmail.com>
2023-06-29 09:40:42 +0100

    update

--- pages/commandes/index.js
+++ pages/commandes/index.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCommandes } from '../../store/commandes/actionCommande';
 import { useRouter } from 'next/router'
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 import RegistrationForm from "../../components/RegistrationForm"
 import Cart from "../../components/Cart"
@@ -15,13 +15,28 @@ import { ADD_PRODUCT_CART } from '../../store/product/type'
 import styles from './style.module.scss'
 import HashLoader from "react-spinners/HashLoader";
 import Layout from '../../components/Layout';
-import { Table, TableCell, TableHead, TableRow } from '@mui/material';
+import { Box, Chip, Grid, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import moment from 'moment';
+import Image from 'next/image';
+import { Popover } from 'antd';
 
-
+const ProductDetail = (product) => (<Box sx={{width: 150}}>
+    <Box sx={{width: 150, height: 100, position: 'relative'}}>
+        <Image 
+            src={`/api/imageproxy?url=${encodeURIComponent(product.product.image)}`}
+            layout='fill'
+            className={styles.card_body_img}
+        />
+    </Box>
+    <Typography>{product.product.name.substring(0,20)}</Typography>
+    <Typography color='orange'>{product.product.price} €</Typography>
+</Box>)
 
 const Commandes = () => {
 
     const [show, setShow] = useState(false)
+    const [showArrow, setShowArrow] = useState(true);
+    const [arrowAtCenter, setArrowAtCenter] = useState(false);
     const dispatch = useDispatch()
     const router = useRouter()
     const [isShowCheckout, setIsShowCheckout] = useState(false)
@@ -34,17 +49,13 @@ const Commandes = () => {
       dispatch(fetchCommandes(user._id)); // Appel de l'action pour récupérer les commandes de l'utilisateur connecté
     }, []);
 
-    const onShowCart = () => {
-        setShow(true)
-    }
+    const mergedArrow = useMemo(() => {
+        if (arrowAtCenter) return { pointAtCenter: true };
+        return showArrow;
+      }, [showArrow, arrowAtCenter]);
 
     const onCloseCheckout = () => {
         setIsShowCheckout(false)
-    }
-
-    const onClearCart = () => {
-        setShow(false)
-        dispatch({type: `${REMOVE__ALL_PRODUCT_CART}_SUCCESS`, payload: {}})
     }
 
     console.log("data", data);
@@ -66,7 +77,56 @@ const Commandes = () => {
                 isVisible={isShowCheckout}
                 onClose={onCloseCheckout}
             />
-            
+            <Typography mb={3} variant='h6'>Mes commandes</Typography>
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow sx={{borderWidth: 1, borderColor: '#000', marginBottom: 2}}>
+                            <TableCell>No</TableCell>
+                            <TableCell align="left">Adresse</TableCell>
+                            <TableCell align="left">Date</TableCell>
+                            <TableCell align="left">Status</TableCell>
+                            <TableCell align="left">Methode de livraison</TableCell>
+                            <TableCell align="left">Quantité de produit</TableCell>
+                            <TableCell align="left">Produits</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            data.length > 0? data.map((command, index) => (
+                                <TableRow>
+                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell>{command.address}</TableCell>
+                                    <TableCell>{moment(command.createdAt).format('DD MMM YYYY')}</TableCell>
+                                    <TableCell><Chip sx={{color: 'black', backgroundColor: 'yellow'}} label={command.status} /></TableCell>
+                                    <TableCell>{command.shipping_method.designation}</TableCell>
+                                    <TableCell>{command.products.reduce((result, product) => result + product.quantity, 0)}</TableCell>
+                                    <TableCell>
+                                        <Grid container>
+                                            {
+                                                command.products.map((product) => (
+                                                    <Grid item>
+                                                        <Box sx={{position: 'relative', height: 40, width: 40, borderRadius: 20, backgroundColor: '#eee', padding: 2, margin: 1}}>
+                                                            <Popover placement="bottom" content={() => ProductDetail(product)} arrow={mergedArrow}>
+                                                                <Image 
+                                                                    src={`/api/imageproxy?url=${encodeURIComponent(product.product.image)}`}
+                                                                    layout='fill'
+                                                                    className={styles.card_body_img}
+                                                                />
+                                                            </Popover>
+                                                        </Box>
+                                                    </Grid>
+                                                ))
+                                            }
+                                        </Grid>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                            :null
+                        }
+                    </TableBody>
+                </Table>
+            </Paper>
         </div>
        </Layout>
     )
